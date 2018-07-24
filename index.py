bd665e2b10d75c763e5874f7da4e18837148efd9
kaikai-wang <38397271+kaikai-wang@users.noreply.github.com>
2018-07-24 14:16:44 +0800

    pic

--- index.py
+++ index.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'index.py'
 
 
 @app.route('/login/')
 def login():
-    return render_template('login.html')
+    return 'login page'
 
 if __name__ == '__main__':
     app.run(port=80, debug=True)
